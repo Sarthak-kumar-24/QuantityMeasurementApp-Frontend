347cc9223312d83f4ee3387cccd9987aa96d1930
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-04-01 23:20:22 +0530

    [Sarthak]: modified auth main class

--- quantity-measurement-frontend/src/main.jsx
+++ quantity-measurement-frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import "./styles/global.css"; // global styles
 import "./index.css"; // optional extra
@@ -9,11 +10,16 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { AuthProvider } from "./store/providers/AuthProvider";
 
+const GOOGLE_CLIENT_ID =
+  import.meta.env.VITE_GOOGLE_CLIENT_ID;;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <AuthProvider>
-        <App />
+        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+          <App />
+        </GoogleOAuthProvider>
       </AuthProvider>
     </Provider>
   </React.StrictMode>,
